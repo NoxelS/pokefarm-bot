4ddaac5488f622bfc26110f74e6ded1e64793248
Michael Weber <michael.weber1337@gmail.com>
2021-05-30 21:25:52 +0200

    Add working dexchecker

--- src/controllers/dexchecker.ts
+++ src/controllers/dexchecker.ts
@@ -3,7 +3,7 @@ import {filter, map, mergeMap, pairwise, switchMap} from 'rxjs/operators';
 import {RequestMethod, sendServerRequestAndGetHtml} from '../utils/requests';
 import {Pokedex} from "../shared/items.const";
 
-export interface PokedexEntry {
+export class PokedexEntry {
     id: string;
     name: string;
     type1: number;
@@ -15,19 +15,49 @@ export interface PokedexEntry {
     shinydex: boolean;
     albidex: boolean;
     melandex: boolean;
-}
 
-function getDexValueFromEntry(dex: Pokedex, entry: PokedexEntry): boolean {
-    if (dex == Pokedex.egg) return entry.eggdex;
-    if (dex == Pokedex.pkmn) return entry.pokedex;
-    if (dex == Pokedex.shiny) return entry.shinydex;
-    if (dex == Pokedex.albi) return entry.albidex;
-    if (dex == Pokedex.melan) return entry.melandex;
-    return false;
+    constructor(id: string, name: string, type1: number, type2: number, eggs: boolean, eggdex: boolean, pkmn: boolean, pokedex: boolean, shinydex: boolean, albidex: boolean, melandex: boolean) {
+        this.id = id;
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.eggs = eggs;
+        this.eggdex = eggdex;
+        this.pkmn = pkmn;
+        this.pokedex = pokedex;
+        this.shinydex = shinydex;
+        this.albidex = albidex;
+        this.melandex = melandex;
+    }
+
+    getDexValueFromEntry(dex: Pokedex): boolean {
+        if (dex == Pokedex.egg) return this.eggdex;
+        if (dex == Pokedex.pkmn) return this.pokedex;
+        if (dex == Pokedex.shiny) return this.shinydex;
+        if (dex == Pokedex.albi) return this.albidex;
+        if (dex == Pokedex.melan) return this.melandex;
+        return false;
+    }
+
+    static ofArray(arr: Array<any>): PokedexEntry {
+        return new PokedexEntry(
+            arr[0],
+            arr[1],
+            arr[2],
+            arr[3],
+            arr[4],
+            arr[5],
+            arr[6],
+            arr[7],
+            arr[8],
+            arr[9],
+            arr[10],
+        )
+    }
 }
 
 function notInDex(dex: Pokedex): (mon: PokedexEntry) => boolean {
-    return (mon: PokedexEntry) => !getDexValueFromEntry(dex, mon);
+    return (mon: PokedexEntry) => !mon.getDexValueFromEntry(dex);
 }
 
 function getName() {
@@ -41,17 +71,21 @@ export function getMissingPokedexEntries(dex: Pokedex): Observable<string> {
     );
 }
 
+function retainPreStageOfAPokemonIfMissingInDex(dex: Pokedex) {
+    return mergeMap(([preStage, pokemon]) => {
+            if (!pokemon.getDexValueFromEntry(dex)) {
+                return Array.of(preStage);
+            } else {
+                return [] as Array<PokedexEntry>
+            }
+        }
+    );
+}
+
 export function getPreStageOfMissingPokedexEntries(dex: Pokedex): Observable<string> {
     return getPokedexEntries().pipe(
         pairwise(),
-        mergeMap(([preStage, pokemon]) => {
-                if (!getDexValueFromEntry(dex, pokemon)) {
-                    return Array.of(preStage);
-                } else {
-                    return [] as Array<PokedexEntry>
-                }
-            }
-        ),
+        retainPreStageOfAPokemonIfMissingInDex(dex),
         map(getName())
     );
 }
@@ -60,8 +94,9 @@ export function getPokedexEntries(): Observable<PokedexEntry> {
     const requestURL = 'https://pokefarm.com/dex';
     return sendServerRequestAndGetHtml(requestURL, RequestMethod.Get).pipe(
         switchMap(html => {
-            const allPokemon = Object.values(JSON.parse(html.querySelector("#dexdata").rawText).regions);
-            return ([] as Array<any>).concat(...allPokemon); //any or PokedexEntry?
-        })
+            let regions: Array<any> = Object.values(JSON.parse(html.querySelector("#dexdata").rawText).regions);
+            return ([] as Array<any>).concat(...regions);
+        }),
+        map(plainArray => PokedexEntry.ofArray(plainArray))
     );
 }
